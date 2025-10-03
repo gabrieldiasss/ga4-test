3813544cad81933376c05030f998748e107f7dc8
gabrieldiasss <gabrieldiascorrea7@gmail.com>
2025-10-03 13:20:59 -0300

    error build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
-import { NextScript } from "next/document";
 import Script from "next/script";
 
 const geistSans = Geist({
